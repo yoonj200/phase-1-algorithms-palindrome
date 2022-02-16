7e76adecb637b1eb6c542f7bbe0570a3c84dac16
Joshua <yoonj200@gmail.com>
2022-02-16 12:54:11 -0600

    commit tree 26e730c03162e5d69bd6dcb6e32a6a4a0c26ff52

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+const { expect } = require("chai");
+
+// Write algorithm here
 function isPalindrome(word) {
-  // Write your algorithm here
+  const arrValues = word.split(''); 
+  // split('') divides string into ordered list, 
+  // puts substrings into an array, and returns it
+  const reverseArrValues = arrValues.reverse(); 
+  const reverseWord = reverseArrValues.join(''); 
+  //  join('') method creates + returns new string by 
+  //  concatenating all array elements
+  if(word === reverseWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
-  Add your pseudocode here
-*/
+Make a function that returns true if string is palindrome
+and false if not a palindrome.  
 
-/*
-  Add written explanation of your solution here
+convert string to an array
+convert array to a string
+convert array BACK to string
+
+if original string === new string, return true
+  else return false
+
 */
 
 // You can run `node index.js` to view these console logs
